dd03fd79257aefbb9ddc0baca2e916394794e816
Nick Andrulewicz <44916311+nzandrulewicz@users.noreply.github.com>
2022-03-30 21:54:01 -0400

    Update MenuClient.java

--- Object Oriented Design and Programming/Assignments/3 - Implementing and Using Iterators/MenuClient.java
+++ Object Oriented Design and Programming/Assignments/3 - Implementing and Using Iterators/MenuClient.java
@@ -8,14 +8,49 @@ public class MenuClient
 		
 		nicksMenu.add("Lobster Dinner", Menu.MAIN_DISH, Menu.NOT_HEART_HEALTHY, "24.99");
 		nicksMenu.add("Rice Pudding", Menu.DESSERT, Menu.NOT_HEART_HEALTHY, "3.50");
-		nicksMenu.add("12oz NY Strip", Menu.MAIN_DISH, Menu.NOT_HEART_HEALTHY, "$24.99");
-		nicksMenu.add("Chips and Beer Cheese", Menu.APPETIZERS, Menu.NOT_HEART_HEALTHY, "$6.99");
-	
-		Iterator<Object> arrayListIterator = nicksMenu.iterator();
+		nicksMenu.add("12oz NY Strip", Menu.MAIN_DISH, Menu.NOT_HEART_HEALTHY, "24.99");
+		nicksMenu.add("Chips and Beer Cheese", Menu.APPETIZERS, Menu.NOT_HEART_HEALTHY, "6.99");
+		nicksMenu.add("Grilled Salmon", Menu.MAIN_DISH, Menu.HEART_HEALTHY, "14.99");
 		
-		while(arrayListIterator.hasNext())
-    {
-    	System.out.println(arrayListIterator.next() + " ");
-    }
+		actionMenu(nicksMenu);
+	}
+	
+	public static void actionMenu(Menu nicksMenu)
+	{
+		try (Scanner cin = new Scanner(System.in)) {
+			System.out.println("ACTION MENU");
+			System.out.println();
+			System.out.println("1) Display All Menu Items");
+			System.out.println();
+			System.out.print("Enter action number: ");
+			int userChoice = cin.nextInt();
+			
+			switch(userChoice)
+			{
+				case 1:
+					System.out.println();
+					displayAllMenuItems(nicksMenu);
+			}
+		}
+	}
+	
+	public static void displayAllMenuItems(Menu nicksMenu)
+	{
+		MenuItem menuItem;
+		MenuIterator itr = nicksMenu.getAllItemsIterator();
+		
+		System.out.println("============================");
+		System.out.println("ALL MENU ITEMS");
+		System.out.println("----------------------------");
+		while (itr.hasNext())
+		{
+			menuItem = itr.next();
+			System.out.println(menuItem.getFoodName() + " $" + menuItem.getFoodPrice());
+		}
+		System.out.println("----------------------------");
+		System.out.println("============================");
+		System.out.println();
+		
+		actionMenu(nicksMenu);
 	}
 }
